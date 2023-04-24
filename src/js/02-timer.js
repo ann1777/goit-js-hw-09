34fdf19aa8a8442afec6ae2d73dd0e034849e46a
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-24 16:35:25 +0300

    Fixed 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,15 @@
 
 import flatpickr from "flatpickr";
-
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
 const calendar = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
-const daysEl = document.querySelector('[data-days]');
-const hoursEl = document.querySelector('[data-hours]');
-const minutesEl = document.querySelector('[data-minutes]');
-const secondsEl = document.querySelector('[data-seconds]');
-const DELAY = 1000;
-startBtn.disabled = 'disabled';
+const TIMER_DELAY = 1000;
+startBtn.disabled = true;
+let intervalId = null;
+let selectedDate = null;
+let currentDate = null;
 
 Notiflix.Notify.info(
     'Please, choose a date and click on start',
@@ -23,9 +21,9 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0]
+    selectedDate = selectedDates[0]
     const now = new Date();
-    if (selectedDate.getTime < now.getTime) {
+    if (selectedDate < now) {
       Notiflix.Notify.failure("Please choose a date in the future")
       startBtn.setAttribute('disabled', true);
       calendar.style.borderColor = "red";
@@ -33,46 +31,50 @@ const options = {
       console.log(selectedDate);
       startBtn.removeAttribute('disabled');
       Notiflix.Notify.success('Goog! You chose a date in the future');
-      startBtn.addEventListener('click', Timer.timerOn);
+      startBtn.addEventListener('click', timer.timerOn);
       calendar.style.borderColor = "green";
+      startBtn.disabled = false;
+      const setTimer = () => {
+        selectedDate = selectedDates[0].getTime();
+        timer.start();
+      };
+      startBtn.addEventListener('click', setTimer);
     }
   },
 };
 
-const fltpkr = flatpickr(calendar, options);
+flatpickr(calendar, options);
 
-class Timer {
-  constructor({onReboot, dataStart = 0, onStop = null}) {
-    this.dataStart = dataStart;
-    this.intervalId = null;
-    this.selectedDate = fltpkr._selectedDateObj;
-    this.onReboot = onReboot;
-    this.onStop = onStop;
-  };
+const timer = {
+  rootSelector: document.querySelector(".timer"),
   
   start() {
-    if(this.isActive) {
-      return;
-    }
-    const startTime = selectedDate.getTime;
-    const curTime = Date.now();
-    console.log(curTime);
-    const deltaTime = startTime - curTime;
-    this.isActive = true;
+    intervalId = setInterval(() => {
+      startBtn.disabled = true;
+      calendar.disabled = true;
+      currentDate = Date.now();
+      const deltaTime = selectedDate - currentDate;
 
-    this.intervslId = setInterval(() => {
       if((deltaTime) < 0) {
-        clearInterval(this.intervalId);
+        clearInterval(intervalId);
+        this.intervalId = null;
         startBtn.disabled = true;
         calendar.disabled = false;
         Notiflix.Notify.failure("Please choose a date in the future")
         return;
       }
-      const time = this.convertMs(deltaTime);
-      this.onReboot(time);
-    }, DELAY);
-    startBtn.disabled = 'disabled';
-  };
+      const { days, hours, minutes, seconds } = this.convertMs(deltaTime);
+      this.rootSelector.querySelector('[data-days]').textContent =
+        this.addLeadingZero(days);
+      this.rootSelector.querySelector('[data-hours]').textContent =
+        this.addLeadingZero(hours);
+      this.rootSelector.querySelector('[data-minutes]').textContent =
+        this.addLeadingZero(minutes);
+      this.rootSelector.querySelector('[data-seconds]').textContent =
+        this.addLeadingZero(seconds);
+    }, TIMER_DELAY);
+    // startBtn.disabled = 'disabled';
+  },
 
   convertMs(ms) {
     const second = 1000;
@@ -86,24 +88,9 @@ class Timer {
     const seconds = this.addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
     return { days, hours, minutes, seconds };
-  }
+  },
   
   addLeadingZero(value) {
     return String(value).padStart(2, '0');
-  };
+  },
 };
-
-const timer = new Timer({
-  onReboot: updateTimerData,
-});
-
-function updateTimerData ({ days, hours, minutes, seconds }) {
-  daysEl.textContent = `${days}`;
-  hoursEl.textContent = `${hours}`;
-  minutesEl.textContent = `${minutes}`;
-  secondsEl.textContent = `${seconds}`;
-};
-
-startBtn.addEventListener("click", timer.start());
-
-//Функція convertMs() повертає об'єкт з розрахованим часом, що залишився до кінцевої дати. Зверни увагу, що вона не форматує результат. Тобто, якщо залишилося 4 хвилини або будь-якої іншої складової часу, то функція поверне 4, а не 04. В інтерфейсі таймера необхідно додавати 0, якщо в числі менше двох символів. Напиши функцію addLeadingZero(value), яка використовує метод padStart() і перед рендерингом інтефрейсу форматує значення.
