30394e985ca5c9786d1211760756ce8e1048d152
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-25 17:21:26 +0300

    Fixed promises failure notifications

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -14,7 +14,7 @@ function createPromise(position, stepDelay) {
       } else {
         return reject({ position, stepDelay });
       }
-    }, firstDelay)
+    }, stepDelay)
 });
 }
 
@@ -23,29 +23,24 @@ function onCreatePromisesClick(e) {
 
   const formEl = e.target.elements;
 
-  if (formEl.delay.value < 0 || formEl.amount.value <= 0) {
+  if (formEl.delay.value < 0 || formEl.amount.value <= 0 || formEl.step.value < 0) {
     return Notiflix.Notify.warning("Delay step and amount values must be positive numbers and the amount value must be more than zero");
 } 
-
+ 
   const firstDelay = Number(formEl.delay.value);
   const delayStep = Number(formEl.step.value);
   const inputAmount = Number(formEl.amount.value);
- /*  console.log(firstDelay);
-  console.log(delayStep);
-  console.log(inputAmount); */
 
   for (let position = 0; position < inputAmount; position += 1) {
     let stepDelay = firstDelay + delayStep * position;
     console.log(stepDelay);
     
-    createPromise(position, stepDelay)
+    createPromise(position+1, stepDelay)
       .then(({ position, stepDelay }) => {
-
           Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${stepDelay}ms`);
     })
-      .catch(({ position, stepDelay }) => {
-            
-          Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${stepDelay}ms`);
+      .catch(({ position, stepDelay }) => {    
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${stepDelay}ms`);
       })
   }
 }
